4e4487180e4cefbc7b819972d9f51a39d5901f6d
Jeffrey Meng <jeffkmeng@gmail.com>
2020-04-26 02:51:20 -0700

    feat: add auth template so page can be used as a template

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,28 @@
 import React, { ReactElement } from "react";
 import { Layout, SEO } from "../components/layout";
 import { WindowLocation } from "@reach/router";
+import { navigate } from "gatsby";
+import { AuthContinueState } from "../components/auth";
+import { useFirebase } from "../firebase";
+import AuthContext from "../context/AuthContext";
 
 interface PageProps {
 	location: WindowLocation;
 }
 
 export default function NotFoundPage({ location }: PageProps): ReactElement {
+	// const {user, loading, error} = React.useContext(AuthContext);
+	// const firebase = useFirebase();
+	// if (!loading && !user) {
+	// 	navigate("/account/login", {
+	// 		state: {
+	// 			from: "/404",
+	// 			message: true,
+	// 			return: true,
+	// 		} as AuthContinueState,
+	// 	});
+	// }
+
 	return (
 		<Layout location={location}>
 			<SEO title="404: Not found" />
